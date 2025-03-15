41649410665e168e9bbae137ef795caa21aa9da1
blagoySimandov <baggy.simandoff@gmail.com>
2025-03-15 21:25:23 +0000

    feat: dijkstra benchmark via timeit

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,5 +1,14 @@
 from xcollections.graph import Graph
+from timeit import timeit
+
 
 g = Graph()
-m = g.generate_random_graph(4, 4)
-g.dijkstra(m[0][0], m[3][3], True)
+m = g.generate_random_graph(500, 500)
+
+
+def dijkstra_wrapper():
+    g.dijkstra(m[0][0], m[499][499], True)
+
+
+execution_time = timeit(dijkstra_wrapper, number=10)
+print(f"Execution time: {execution_time / 10}")
